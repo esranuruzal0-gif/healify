87a94236d4ea15e8b394685f4268d1940fd80a71
esranuruzal0-gif <esranuruzal0@gmail.com>
2026-05-27 17:59:03 +0300

    healify is created and entity is added for app

---
+++ src/main/java/org/example/healify/HealifyApplication.java
@@ -0,0 +1,13 @@
+package org.example.healify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealifyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/healify/entity/healthyrecipe.java
@@ -0,0 +1,84 @@
+package org.example.healify.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class healthyrecipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private int calories;
+    private int prepTime;
+    private boolean healthy;
+
+    public healthyrecipe() {
+    }
+
+    public healthyrecipe(Long id, String title, String description, int calories, int prepTime, boolean healthy) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.calories = calories;
+        this.prepTime = prepTime;
+        this.healthy = healthy;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(int prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public boolean isHealthy() {
+        return healthy;
+    }
+
+    public boolean getHealthy() {
+        return healthy;
+    }
+
+    public void setHealthy(boolean healthy) {
+        this.healthy = healthy;
+    }
+}
